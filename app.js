f48b0760053b2d25a9dcaeaccdeb112846948f44
Rakesh M R <rakesh.mr@signzy.com>
2020-03-16 15:24:39 +0530

    added controllers

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const product = require('./routes/product.route')
+app.use('/products', product)
+
+
 let port = 3000
 
 app.listen(port, () => {
